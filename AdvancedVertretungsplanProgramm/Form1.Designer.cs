33cd0e4a91d19867558400d9a9f89e264dece096
RobertK <github@robert-k.net>
2018-04-07 20:25:29 +0200

    Version Beta 0.8

---
+++ AdvancedVertretungsplanProgramm/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace AdvancedVertretungsplanProgramm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.loadTableTomorrow = new System.Windows.Forms.Button();
+            this.lessonName = new System.Windows.Forms.TextBox();
+            this.addLesson = new System.Windows.Forms.Button();
+            this.saveData = new System.Windows.Forms.Button();
+            this.LoadTableToday = new System.Windows.Forms.Button();
+            this.Header = new System.Windows.Forms.Label();
+            this.Mitteilungen = new System.Windows.Forms.TextBox();
+            this.madeBy = new System.Windows.Forms.Label();
+            this.ShowLessons = new System.Windows.Forms.Button();
+            this.deleteLessons = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.BackgroundColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.dataGrid.Location = new System.Drawing.Point(12, 156);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.Size = new System.Drawing.Size(906, 273);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // loadTableTomorrow
+            // 
+            this.loadTableTomorrow.Location = new System.Drawing.Point(12, 479);
+            this.loadTableTomorrow.Name = "loadTableTomorrow";
+            this.loadTableTomorrow.Size = new System.Drawing.Size(165, 38);
+            this.loadTableTomorrow.TabIndex = 1;
+            this.loadTableTomorrow.Text = "Vertretungsplan von morgen laden";
+            this.loadTableTomorrow.UseVisualStyleBackColor = true;
+            this.loadTableTomorrow.Click += new System.EventHandler(this.loadTableTomorrow_Click);
+            // 
+            // lessonName
+            // 
+            this.lessonName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lessonName.Location = new System.Drawing.Point(12, 435);
+            this.lessonName.Multiline = true;
+            this.lessonName.Name = "lessonName";
+            this.lessonName.Size = new System.Drawing.Size(165, 38);
+            this.lessonName.TabIndex = 2;
+            this.lessonName.Text = "Kursname";
+            this.lessonName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lessonName_KeyDown);
+            // 
+            // addLesson
+            // 
+            this.addLesson.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.addLesson.Location = new System.Drawing.Point(184, 435);
+            this.addLesson.Name = "addLesson";
+            this.addLesson.Size = new System.Drawing.Size(111, 38);
+            this.addLesson.TabIndex = 3;
+            this.addLesson.Text = "Kurs Hinzufügen";
+            this.addLesson.UseVisualStyleBackColor = true;
+            this.addLesson.Click += new System.EventHandler(this.addLesson_Click);
+            // 
+            // saveData
+            // 
+            this.saveData.Location = new System.Drawing.Point(807, 462);
+            this.saveData.Name = "saveData";
+            this.saveData.Size = new System.Drawing.Size(111, 38);
+            this.saveData.TabIndex = 4;
+            this.saveData.Text = "Kurse Abspeichern";
+            this.saveData.UseVisualStyleBackColor = true;
+            this.saveData.Click += new System.EventHandler(this.saveData_Click);
+            // 
+            // LoadTableToday
+            // 
+            this.LoadTableToday.Location = new System.Drawing.Point(184, 479);
+            this.LoadTableToday.Name = "LoadTableToday";
+            this.LoadTableToday.Size = new System.Drawing.Size(165, 38);
+            this.LoadTableToday.TabIndex = 6;
+            this.LoadTableToday.Text = "Vertretungsplan von heute laden";
+            this.LoadTableToday.UseVisualStyleBackColor = true;
+            this.LoadTableToday.Click += new System.EventHandler(this.LoadTableToday_Click);
+            // 
+            // Header
+            // 
+            this.Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Header.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Header.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Header.Location = new System.Drawing.Point(0, 0);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(930, 34);
+            this.Header.TabIndex = 7;
+            this.Header.Text = "Vertretungsplan für ...";
+            // 
+            // Mitteilungen
+            // 
+            this.Mitteilungen.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Mitteilungen.Location = new System.Drawing.Point(13, 38);
+            this.Mitteilungen.Multiline = true;
+            this.Mitteilungen.Name = "Mitteilungen";
+            this.Mitteilungen.ReadOnly = true;
+            this.Mitteilungen.Size = new System.Drawing.Size(905, 112);
+            this.Mitteilungen.TabIndex = 8;
+            // 
+            // madeBy
+            // 
+            this.madeBy.AutoSize = true;
+            this.madeBy.Location = new System.Drawing.Point(800, 503);
+            this.madeBy.Name = "madeBy";
+            this.madeBy.Size = new System.Drawing.Size(118, 13);
+            this.madeBy.TabIndex = 9;
+            this.madeBy.Text = "Made by Robert Kalmar";
+            this.madeBy.Click += new System.EventHandler(this.madeBy_Click);
+            // 
+            // ShowLessons
+            // 
+            this.ShowLessons.Location = new System.Drawing.Point(355, 479);
+            this.ShowLessons.Name = "ShowLessons";
+            this.ShowLessons.Size = new System.Drawing.Size(111, 38);
+            this.ShowLessons.TabIndex = 10;
+            this.ShowLessons.Text = "Kurse Anzeigen";
+            this.ShowLessons.UseVisualStyleBackColor = true;
+            this.ShowLessons.Click += new System.EventHandler(this.ShowLessons_Click);
+            // 
+            // deleteLessons
+            // 
+            this.deleteLessons.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.deleteLessons.Location = new System.Drawing.Point(301, 434);
+            this.deleteLessons.Name = "deleteLessons";
+            this.deleteLessons.Size = new System.Drawing.Size(111, 38);
+            this.deleteLessons.TabIndex = 11;
+            this.deleteLessons.Text = "Kurs Entfernen";
+            this.deleteLessons.UseVisualStyleBackColor = true;
+            this.deleteLessons.Click += new System.EventHandler(this.deleteLessons_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 525);
+            this.Controls.Add(this.deleteLessons);
+            this.Controls.Add(this.ShowLessons);
+            this.Controls.Add(this.madeBy);
+            this.Controls.Add(this.Mitteilungen);
+            this.Controls.Add(this.Header);
+            this.Controls.Add(this.LoadTableToday);
+            this.Controls.Add(this.saveData);
+            this.Controls.Add(this.addLesson);
+            this.Controls.Add(this.lessonName);
+            this.Controls.Add(this.loadTableTomorrow);
+            this.Controls.Add(this.dataGrid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Advanced Vertretungsplan Programm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button loadTableTomorrow;
+        private System.Windows.Forms.TextBox lessonName;
+        private System.Windows.Forms.Button addLesson;
+        private System.Windows.Forms.Button saveData;
+        private System.Windows.Forms.Button LoadTableToday;
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.TextBox Mitteilungen;
+        private System.Windows.Forms.Label madeBy;
+        private System.Windows.Forms.Button ShowLessons;
+        private System.Windows.Forms.Button deleteLessons;
+    }
+}
+
---
+++ AdvancedVertretungsplanProgramm/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace AdvancedVertretungsplanProgramm
+{
+    public partial class Form1 : Form
+    {
+        private List<string> Kurse = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+        
+        /// <summary>
+        /// Erstellt und füllt das DataGrid
+        /// </summary>
+        /// <param name="Website">Website von der die Dateien geholt werden sollen (morgen oder übermorgen)</param>
+        private void FillDataGrid(string Website)
+        {
+            if (AutoLoadLessons() && CheckNet())
+            {
+                int Rows = 1;
+                bool vertret = false;
+                dataGrid.ColumnCount = 8;
+                dataGrid.RowCount = Rows;
+
+                var UngeradeZelle = new List<HtmlNode>();
+                var GeradeZelle = new List<HtmlNode>();
+
+                List<string> Strings = new List<string>();
+
+                HtmlWeb web = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = web.Load(Website);
+
+                if (doc.DocumentNode.SelectSingleNode("//h2[@class='TextUeberschrift']") != null)
+                {
+                    Header.Text = doc.DocumentNode.SelectSingleNode("//h2[@class='TextUeberschrift']").InnerText;
+
+                    if (doc.DocumentNode.SelectSingleNode("//table[@class='TabelleMitteilung']") != null)
+                    {
+                        Mitteilungen.Text = doc.DocumentNode.SelectSingleNode("//table[@class='TabelleMitteilung']").InnerText.Remove(0, 3).ToString();
+                    }
+                    else
+                    { Mitteilungen.Text = null; }
+
+                    if (doc.DocumentNode.SelectNodes("//td[@class='UngeradeZelleTabelleVertretungen']") != null)
+                    {
+                        UngeradeZelle = doc.DocumentNode.SelectNodes("//td[@class='UngeradeZelleTabelleVertretungen']").ToList();
+                    }
+
+                    if (doc.DocumentNode.SelectNodes("//td[@class='ZelleGeradeTabelleVertretungen']") != null)
+                    {
+                        GeradeZelle = doc.DocumentNode.SelectNodes("//td[@class='ZelleGeradeTabelleVertretungen']").ToList();
+                    }
+                }
+                else
+                {
+                    Header.Text = "Vertretungsplan konnte nicht geladen werden.";
+                }
+
+                foreach (var item in UngeradeZelle)
+                {
+                    Strings.Add(item.InnerText);
+                }
+                foreach (var item in GeradeZelle)
+                {
+                    Strings.Add(item.InnerText);
+                }
+
+                for (int i = 0; i < Strings.Count; i++)
+                {
+                    for (int k = 0; k < Kurse.Count; k++)
+                    {
+                        if (Strings[i].Contains(Kurse[k]))
+                        {
+                            dataGrid.RowCount = Rows++;
+                            dataGrid.Rows.Add(Strings[i], Strings[++i], Strings[++i], Strings[++i], Strings[++i], Strings[++i], Strings[++i], Strings[++i]);
+                            vertret = true;
+                        }
+                    }
+                }
+
+                if (vertret == false)
+                {
+                    dataGrid.Rows.Add("Keine Vertretungen");
+                }
+
+                ColumnName();
+                dataGrid.Sort(dataGrid.Columns[1], System.ComponentModel.ListSortDirection.Ascending);
+            }else {
+                Header.Text = "Error";
+                Mitteilungen.Text = "Bitte Überprüfe deine Internetverbindung.";
+            }
+        }
+
+        /// <summary>
+        /// Speichert die Kurse in einer JSON Datei ab
+        /// </summary>
+        private void SaveDataInJson()
+        {
+            using (StreamWriter file = File.CreateText(@"Kurse.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, Kurse);
+            }
+        }
+
+        /// <summary>
+        /// Lädt die Kurse aus einer JSON Datei
+        /// </summary>
+        private void LoadJson()
+        {
+            Kurse.Clear();
+
+            using (StreamReader reader = new StreamReader("Kurse.json"))
+            {
+                string json = reader.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+
+                foreach(var item in array)
+                {
+                    Kurse.Add(item.ToString());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gibt den Spalten Namen
+        /// </summary>
+        private void ColumnName()
+        {
+            dataGrid.Columns[0].Name = "Klasse";
+            dataGrid.Columns[1].Name = "Stunde";
+            dataGrid.Columns[2].Name = "Vertretung";
+            dataGrid.Columns[3].Name = "Fach";
+            dataGrid.Columns[4].Name = "statt";
+            dataGrid.Columns[5].Name = "Lehrer";
+            dataGrid.Columns[6].Name = "Raum";
+            dataGrid.Columns[7].Name = "Sonstiges";
+        }
+
+        /// <summary>
+        /// Lädt den Vertretungsplan von morgen
+        /// </summary>
+        private void loadTableTomorrow_Click(object sender, EventArgs e)
+        {
+            FillDataGrid("http://www.hans-sachs-gymnasium.de/WocheHP/schuelerplan_morgen.htm");
+        }
+        
+        /// <summary>
+        /// Lädt den Vertretungsplan von heute
+        /// </summary>
+        private void LoadTableToday_Click(object sender, EventArgs e)
+        {
+            FillDataGrid("http://www.hans-sachs-gymnasium.de/WocheHP/schuelerplan_heute.htm");
+        }
+        
+        private void addLesson_Click(object sender, EventArgs e)
+        {
+            AddLessons();
+        }
+        
+        private void lessonName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                AddLessons();
+            }
+        }
+
+        /// <summary>
+        /// Fügt Kurse der Liste Kurse hinzu
+        /// </summary>
+        private void AddLessons()
+        {
+            Kurse.Add(lessonName.Text);
+            lessonName.ResetText();
+        }
+
+        /// <summary>
+        /// Überprüft ob eine Kursliste bereits besteht und lädt sie dann
+        /// </summary>
+        /// <returns>Gibt einen Boolean ob die Kursliste besteht zurück</returns>
+        private bool AutoLoadLessons()
+        {
+            if (File.Exists(@"Kurse.json"))
+            {
+                LoadJson();
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Es besteht keine Kursliste. Erstelle eine Kursliste und speichere sie ab.");
+                return false;
+            }
+        }
+
+        private void saveData_Click(object sender, EventArgs e)
+        {
+            SaveDataInJson();
+            MessageBox.Show("Die Kurse wurden erfolgreich in Kurse.json abgespeichert.");
+        }
+
+        private void madeBy_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Programmiert von Robert Kalmar mithilfe von Microsoft Visual Studio."+ "\n" +"Und der Benutzung von Newtonsoft.Json und HtmlAgilityPack.\nEin Changelog befindet sich auf http://avp.robert-k.net","Version Beta 0.8");
+        }
+
+        /// <summary>
+        /// Zeigt die eingegebenen Kurse in der Tabelle an
+        /// </summary>
+        private void ShowLessons_Click(object sender, EventArgs e)
+        {
+            int Rows = 1;
+            dataGrid.ColumnCount = 1;
+            dataGrid.RowCount = Rows;
+            dataGrid.Columns[0].Name = "Kurse";
+
+            if (AutoLoadLessons())
+            {
+                LoadJson();
+
+                for (int i = 0; i < Kurse.Count; i++)
+                {
+                    dataGrid.RowCount = Rows++;
+                    dataGrid.Rows.Add(Kurse[i]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Löscht den eingegebenen Kurs und speichert die Liste neu ab
+        /// </summary>
+        private void deleteLessons_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Kurse.Count; i++)
+            {
+                if (Kurse[i] == lessonName.Text)
+                {
+                    Kurse.RemoveAt(i);
+                    lessonName.ResetText();
+                    SaveDataInJson();
+                }
+            }
+        }
+
+        [System.Runtime.InteropServices.DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+        public static bool CheckNet()
+        {
+            int desc;
+            return InternetGetConnectedState(out desc, 0);
+        }
+    }
+}
